533df3b67567b189f9a65d6df254e2c70f517cc8
Syed-Isar <syedisar@gmail.com>
2024-09-04 17:47:06 +0500

    updated till the cards displayed with slider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Footer from './components/Footer'
 import FreeBook from './components/FreeBook'
+import Cards from './components/Cards'
 
 function App() {
   return (
