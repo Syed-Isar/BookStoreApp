ef227f0cc4288dc495080be64f4d0d06b2f77407
Syed-Isar <syedisar@gmail.com>
2024-09-05 17:25:56 +0500

    course tab till button

--- Frontend/src/components/Courses.jsx
+++ Frontend/src/components/Courses.jsx
@@ -3,9 +3,26 @@ import React from "react";
 function Courses() {
   return (
     <>
-    <div>Courses</div>;
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className=" text-2xl md:text-4xl ">
+            We are delighted to have you{" "}
+            <span className="text-pink-500">Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus,
+            adipisci error rem maxime officiis molestiae harum. Aliquid repellat
+            repellendus fugiat, architecto minus voluptas delectus perspiciatis.
+            Quisque consequ habitant morbi tr incididunt Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Quod, id dolorem. Amet, dolor
+            enim anra kadarabra for you.
+          </p>
+          <button className="btn bg-pink-500 text-white px-3 py-2 rounded-md mt-6 hover:bg-pink-700 duration-300">Button</button>
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Courses;
