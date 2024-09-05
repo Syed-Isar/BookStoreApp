7951d002e3ff72b0bf748b27facf11024b548814
Syed-Isar <syedisar@gmail.com>
2024-09-05 10:12:58 +0500

    Home page completed

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -5,8 +5,8 @@ function Cards({item}) {
   console.log(item)
   return (
     <>
-      <div className="mt-4">
-        <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="mt-4 p-3" >
+        <div className="card bg-base-100 w-92 shadow-xl">
           <figure>
             <img src={item.img} alt="Books" />
           </figure>
@@ -18,7 +18,7 @@ function Cards({item}) {
             <p>{item.title}</p>
             <div className="card-actions justify-between">
               <div className="badge badge-outline">${item.price}</div>
-              <div className="badge badge-outline hover:bg-pink-500 hover:text-white">Buy Now</div>
+              <div className="badge badge-outline rounded-full p-3 hover:bg-pink-500 hover:text-white">Buy Now</div>
             </div>
           </div>
         </div>
